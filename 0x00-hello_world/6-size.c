97b6bee94d2b963d564e20e07a08b99e936fe294
Abiy-Rike <rikeabiy@gmail.com>
2022-10-13 01:12:59 -0700

    .

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,7 @@
  */
 
 int main (void)
-{char a =A ;
-int b = 6;
-long int c = 44444;
-long long int d = 56788;
-float e = 6.78;
-printf(Size of a char: &zu byte(s)\n", sizeof(char));
+{printf("Size of a char: &zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)]n", sizeof(long int));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
