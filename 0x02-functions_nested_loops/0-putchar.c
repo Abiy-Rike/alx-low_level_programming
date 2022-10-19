c44b94ab573ccbb6b425cb1fb30534289773747c
Abiy Paulos Rike <104836267+Abiy-Rike@users.noreply.github.com>
2022-10-19 20:44:24 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,23 @@
-#include "main.h"
-/**
- * main - print _putchar, followed by new line
- * Return: 0
- */
 
+#include "main.h"
+
+/**
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	putchar(int );
-	putchar('\n');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 
 	return (0);
-}end
+}
